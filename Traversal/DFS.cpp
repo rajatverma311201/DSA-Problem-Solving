577fb880792e6cb7be9f7edbf2dcaff00a26a330
Rajat Verma <rajat311201@gmail.com>
2022-05-05 14:35:46 +0530

    added dfs traversal

--- Traversal/DFS.cpp
+++ Traversal/DFS.cpp
@@ -1,46 +1,32 @@
 #include <iostream>
 #include <vector>
-#include <queue>
 using namespace std;
 
 // defining all the required functions
 void display(vector<int> traversal);
-void bfs(int i, vector<int> &traversal, vector<bool> &vis, vector<int> adj[]);
+void dfs(int i, vector<int> &traversal, vector<bool> &vis, vector<int> adj[]);
 vector<int> graphTraversal(int V, vector<int> adj[]);
 
-// bfs function
-void bfs(int i, vector<int> &traversal, vector<bool> &vis, vector<int> adj[])
+// dfs function
+void dfs(int node, vector<int> &traversal, vector<bool> &vis, vector<int> adj[])
 {
-    queue<int> Q;
+    vis[node] = true;
+    traversal.push_back(node);
 
-    // doing bfs for ith node
-    vis[i] = true;
-    Q.push(i);
-    while (!Q.empty())
+    // loop to call the dfs again for
+    // the node which is not yet visited
+    for (auto it : adj[node])
     {
-        // take out the current front node from the queue
-        int node = Q.front();
-        Q.pop();
-
-        // add the node in traversal array
-        traversal.push_back(node);
-
-        // loop for all the adjacent elements(breadth)
-        // in the list of current node;
-        for (auto it : adj[node])
+        // if the node in the list is not yet visited,
+        // call the dfs for that node
+        if (!vis[it])
         {
-            // if the node in the list is not yet visited,
-            // mark visited and then push in the queue
-            if (!vis[it])
-            {
-                vis[it] = true;
-                Q.push(it);
-            }
+            dfs(it, traversal, vis, adj);
         }
     }
 }
 
-// graph traversal function which call the bfs function
+// graph traversal function which call the dfs function
 vector<int> graphTraversal(int V, vector<int> adj[])
 {
     vector<int> traversal;
@@ -48,12 +34,12 @@ vector<int> graphTraversal(int V, vector<int> adj[])
     // if node starts from 1 then i from 1
     for (int i = 1; i < V; i++)
     {
-        // do bfs for each node which is not yet visited
+        // do dfs for each node which is not yet visited
         if (!vis[i])
         {
-            // call the bfs function which will perform
-            // the bfs for the current passed node
-            bfs(i, traversal, vis, adj);
+            // call the dfs function which will perform
+            // the dfs for the current passed node
+            dfs(i, traversal, vis, adj);
         }
     }
     return traversal;
@@ -64,7 +50,7 @@ int main()
 
 #ifndef ONLINE_JUDGE
     freopen("input.txt", "r", stdin);
-    freopen("outputBFS.txt", "w", stdout);
+    freopen("outputDFS.txt", "w", stdout);
 #endif
 
     int n; // n stores no. of nodes
@@ -87,12 +73,13 @@ int main()
 
     vector<int> traversal = graphTraversal(n, adj);
 
-    // display the bfs traversal
-    cout << "BFS : ";
+    // display the dfs traversal
+    cout << "DFS : ";
     display(traversal);
 
     return 0;
 }
+
 void display(vector<int> trav)
 {
     for (int i = 0; i < trav.size(); i++)
